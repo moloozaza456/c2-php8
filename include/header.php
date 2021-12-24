b46c81662bfb7d64952f8e611eeb7fc1432ce056
Dumrong <dumdumrong13@gmail.com>
2021-12-24 11:28:28 +0700

    inital commit

---
+++ include/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html land="en">
+
+<head>	
+	<meta charset="utf-8">
+	<meta name="viewprot" content="width=device-width, initial-scale=1.0">
+	<title>แลนมาร์ค</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/customer.css">
+</head>
---
+++ include/navbar.php
@@ -0,0 +1,51 @@
+<header>
+		<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6f42c1;">
+			<div class="container">
+				<a class="navbar-brand" href="#">นายดำรงค์</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">หน้าแรก</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								เกี่ยวกับเรา
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="#">ประวัติ</a></li>
+								<li>
+									<hr class="dropdown-divider">
+								</li>
+								<li><a class="dropdown-item" href="#">หน้าที่รับผิดชอบ</a></li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">ทีมงานของเรา</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">ภาพความประทับใจ</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">ติดต่อเรา</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							ลิงเว็บไซต์
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="#">อาชีวะ</a></li>
+								<li><a class="dropdown-item" href="#">ชีวศึกษา</a></li>
+								<li>
+									<hr class="dropdown-divider">
+								</li>
+								<li><a class="dropdown-item" href="#">อาชีวซึกษา</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
